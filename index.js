74d116087861498ef7497937ee9cce5834861dbe
BillyMiles <williammiles201@gmail.com>
2021-04-28 09:26:01 -0400

    commit tree d35d6a34f2491e51c7f5f02255906ce38fb93245

---
+++ index.js
@@ -0,0 +1,9 @@
+const retrunFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0, 1);
+};
+
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2);
+  };
+
+  
